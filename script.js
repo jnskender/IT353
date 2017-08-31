c0aa70ee42c1f2ddcc46b5061d38b2b58b9c4689
John Skender <jnskender@gmail.com>
2017-08-31 16:32:41 -0500

    update file structure

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function render_header() {
           <h1>John Skender</h1>
           <nav class="main_nav">
             <ul>
-              <li><a href="index.html">Home</a></li>
-              <li><a href="assignments.html">Assignments</a></li>
-              <li><a href="#">Resume</a></li>
+              <li><a href="index.html">Home</a></li> <span class="seperator">|</span>
+              <li><a href="assignments/assignments.html">Assignments</a></li><span class="seperator">|</span>
+              <li><a href="#">Resume</a></li><span class="seperator">|</span>
               <li><a href="#">Contact</a></li>
             </ul>
           </nav>
