0875419c3bb7e2d7e7f3e6ba9c9e7376ad6a5e36
John Skender <jnskender@gmail.com>
2017-08-31 17:04:32 -0500

    fix broken links, remove js rendering

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    render_header();
+
 }, false);
-
-
-
-function render_header() {
-    document.getElementById("render_head").innerHTML=`
-    <header class="main_head">
-          <h1>John Skender</h1>
-          <nav class="main_nav">
-            <ul>
-              <li><a href="../index.html">Home</a></li> <span class="seperator">|</span>
-              <li><a href="assignments/assignments.html">Assignments</a></li><span class="seperator">|</span>
-              <li><a href="#">Resume</a></li><span class="seperator">|</span>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </nav>
-        </header>`;
-}
