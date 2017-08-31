f38cb0faef3b321ac24e3bab6fa2f9ad2dc22079
John Skender <jnskender@gmail.com>
2017-08-31 16:48:45 -0500

    fixed broken home link

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function render_header() {
           <h1>John Skender</h1>
           <nav class="main_nav">
             <ul>
-              <li><a href="index.html">Home</a></li> <span class="seperator">|</span>
+              <li><a href="../index.html">Home</a></li> <span class="seperator">|</span>
               <li><a href="assignments/assignments.html">Assignments</a></li><span class="seperator">|</span>
               <li><a href="#">Resume</a></li><span class="seperator">|</span>
               <li><a href="#">Contact</a></li>
